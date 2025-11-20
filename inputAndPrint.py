fe641319a6355ff488055079778ac7f5907ef1fa
Akshat Srivastava <akshatsrivastava625@gmail.com>
2025-11-20 13:12:27 +0530

    input and print task

---
+++ inputAndPrint.py
@@ -0,0 +1,14 @@
+name = input("Please enter your name: ")
+print("Welcome " + name)
+
+print("\n------------------------------------------------------------------------------------------------------------------------\n")
+
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter another number: "))
+
+print("Sum is " + str(num1 + num2) )
+print("Multiplication is " + str(num1 * num2))
+print("Division is " + str(num1 / num2))
+
+print("\n------------------------------------------------------------------------------------------------------------------------\n")
+
